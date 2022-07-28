3758fd42e51b006d67c544748b86ae6881a7cd4d
Michal Peterka <mich.peterka@gmail.com>
2022-07-28 22:57:24 +0200

    CsvProvider

---
+++ WebsiteGenerator/Domain.fs
@@ -0,0 +1,11 @@
+﻿module Domain
+open FSharp.Data
+
+[<Literal>]
+let libraryPath = __SOURCE_DIRECTORY__ + """\res\library.csv"""
+
+type Book = CsvProvider<libraryPath, HasHeaders=true>
+
+let library = Book.Load libraryPath
+
+let books = library.Rows
--- WebsiteGenerator/Program.fs
+++ WebsiteGenerator/Program.fs
@@ -1,25 +1,27 @@
 ﻿open Giraffe.ViewEngine
+open Domain
 
 type Book = {Name: string; Publisher: string; Formats: string list}
 
 let myBooks =
     [
-        {Name = "1,000 Ideas by 100 Manga Artists"; Publisher = "The Quarto Group"; Formats = ["MOBI"; "EPUB"; "PDF"]};
-        {Name = "1,000 Ideas by 100 Manga Artists"; Publisher = "The Quarto Group"; Formats = ["MOBI"; "EPUB"; "PDF"]}
+        { Name = "1,000 Ideas by 100 Manga Artists"; Publisher = "The Quarto Group"; Formats = ["MOBI"; "EPUB"; "PDF"] };
+        { Name = "1,000 Ideas by 100 Manga Artists"; Publisher = "The Quarto Group"; Formats = ["MOBI"; "EPUB"; "PDF"] }
     ]
 
 let model = 
-  html [] [ 
+  html [] [
     body [] [
       h1 [] [ Text "My Library" ]
       ul [] [
         for book in myBooks do
           li [] [
-            div [] [ Text $"{book.Name}"]
-            div [] [ Text $"{book.Publisher}"]  
-            div [] [ Text $"{book.Formats}"]
+            div [] [ Text $"{book.Name}" ]
+            div [] [ Text $"{book.Publisher}" ]  
+            div [] [ Text $"{book.Formats}" ]
           ]
       ]
+      p [] [ Text $"{Seq.head books}"]
     ]
   ]
 
